5b2a1923f3675b4f4a91504774d2ba1238faf677
Bowins <Bowins@live.co.uk>
2019-05-09 09:05:54 +0100

    Firebase User Creation Implemented
    Can now add users through the Sign Up UI

--- app/src/main/java/com/group2/carinsuranceapp/MainActivity.java
+++ app/src/main/java/com/group2/carinsuranceapp/MainActivity.java
@@ -1,5 +1,8 @@
 package com.group2.carinsuranceapp;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -8,6 +11,9 @@ import android.widget.EditText;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
 import org.w3c.dom.Text;
@@ -42,11 +48,46 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         auth = FirebaseAuth.getInstance();
 
         //Check already session, if okay go dashboard.
+
+        if(auth.getCurrentUser() != null)
+            startActivity(new Intent(MainActivity.this,Dashboard.class
+            ));
     }
 
 
     @Override
     public void onClick(View view) {
+        if(view.getId() == R.id.login_btn_forgot_password){
+            startActivity(new Intent(MainActivity.this,ForgotPassword.class));
+            finish();
+        }
+        else if(view.getId() == R.id.login_btn_signup){
+            startActivity(new Intent(MainActivity.this,SignUp.class));
+            finish();
+        }
+        else if(view.getId() == R.id.login_btn_login){
+            loginUser(input_email.getText().toString(),input_password.getText().toString());
+        }
 
+    }
+
+    private void loginUser(String email, final String password) {
+        auth.signInWithEmailAndPassword(email,password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(!task.isSuccessful())
+                        {
+                            if(password.length() < 6)
+                            {
+                                Snackbar snackBar = Snackbar.make(findViewById(R.id.activity_main),"Password length must be over 6", Snackbar.LENGTH_SHORT);
+                                snackBar.show();
+                            }
+                        }
+                        else{
+                            startActivity(new Intent(MainActivity.this, Dashboard.class));
+                        }
+                    }
+                });
     }
 }
