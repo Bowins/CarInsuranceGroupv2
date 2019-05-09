c8e533690586f754f5129b343281a367820091ad
Bowins <Bowins@live.co.uk>
2019-05-09 08:18:56 +0100

    Firebase Auth Implemented
    Login Screen Auth and Checks in place.

--- app/src/main/java/com/group2/carinsuranceapp/MainActivity.java
+++ app/src/main/java/com/group2/carinsuranceapp/MainActivity.java
@@ -2,15 +2,51 @@ package com.group2.carinsuranceapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.firebase.auth.FirebaseAuth;
+
+import org.w3c.dom.Text;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button btnLogin;
+    EditText input_email, input_password;
+    TextView btnSignup, btnForgotPass;
+
+    RelativeLayout activity_main;
+
+    private FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        btnLogin = (Button) findViewById(R.id.login_btn_login);
+        input_email = (EditText)findViewById(R.id.login_email);
+        input_password = (EditText)findViewById(R.id.login_password);
+        btnSignup = (TextView)findViewById(R.id.login_btn_signup);
+        btnForgotPass = (TextView)findViewById(R.id.login_btn_forgot_password);
+        activity_main = (RelativeLayout)findViewById(R.id.activity_main);
+
+        btnSignup.setOnClickListener(this);
+        btnForgotPass.setOnClickListener(this);
+        btnLogin.setOnClickListener(this);
+
+        //init Firebase Auth
+        auth = FirebaseAuth.getInstance();
+
+        //Check already session, if okay go dashboard.
     }
 
-    //hello
-    //test
+
+    @Override
+    public void onClick(View view) {
+
+    }
 }
