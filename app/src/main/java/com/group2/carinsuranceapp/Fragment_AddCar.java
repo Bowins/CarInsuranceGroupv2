aa9e888024b71e61c1b688757dfb88c86f57b1c2
Valeria <vcasadoh@gmail.com>
2019-05-12 19:26:09 +0100

    Added input fields for the car submission

--- app/src/main/java/com/group2/carinsuranceapp/Fragment_AddCar.java
+++ app/src/main/java/com/group2/carinsuranceapp/Fragment_AddCar.java
@@ -7,19 +7,28 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
 public class Fragment_AddCar extends Fragment {
+
+    private EditText registration;
+    private EditText model;
+    private EditText make;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_add_car,null);
+
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+        registration = view.findViewById(R.id.carRegistrationLabel);
+        model = view.findViewById(R.id.carModelLabel);
+        make = view.findViewById(R.id.carMakeLabel);
 
     }
 }
