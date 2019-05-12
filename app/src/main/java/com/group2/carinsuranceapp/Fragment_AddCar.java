16da73c057913fe6cf5b103efec402f553a6dd33
Valeria <vcasadoh@gmail.com>
2019-05-12 19:52:53 +0100

    Fixed a few things

--- app/src/main/java/com/group2/carinsuranceapp/Fragment_AddCar.java
+++ app/src/main/java/com/group2/carinsuranceapp/Fragment_AddCar.java
@@ -1,5 +1,6 @@
 package com.group2.carinsuranceapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -75,6 +76,8 @@ public class Fragment_AddCar extends Fragment {
                         //Setting value
                         //Path is Car -> Current user ID -> Car ID
                         myRef.child(userAuth.getUid()).child(reg).setValue(car);
+
+                        startActivity(new Intent(getContext(), Fragment_MyInsuranceInfo.class));
 
                     } catch (Exception e) {
                         e.printStackTrace();
