9f3438db32a816a6aed9a9e511680497c9092dff
Valeria <vcasadoh@gmail.com>
2019-05-12 16:12:49 +0100

    Error checking for first database input from client

--- app/src/main/java/com/group2/carinsuranceapp/DatabaseInputPersonalInfoActivity.java
+++ app/src/main/java/com/group2/carinsuranceapp/DatabaseInputPersonalInfoActivity.java
@@ -15,6 +15,13 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.group2.databaseclasses.UserData;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 public class DatabaseInputPersonalInfoActivity extends AppCompatActivity {
 
@@ -51,13 +58,55 @@ public class DatabaseInputPersonalInfoActivity extends AppCompatActivity {
         nextButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+                String dateOfBirth = dateOfBirthField.getText().toString();
+
+                try {
+                    Date date1=new SimpleDateFormat("dd/MM/yyyy").parse(dateOfBirth);
+                } catch (ParseException e) {
+                    e.printStackTrace();
+                    Toast.makeText(getApplicationContext(), "Wrong Date Input", Toast.LENGTH_LONG);
+                }
+
+                if ((inputHandle(firstNameField) == false) && (inputHandle(surnameField) == false) && (dateOfBirthField.getText().toString().equals(""))) {
+
+                    //Creating User
+                    String first = firstNameField.getText().toString();
+                    String surname = surnameField.getText().toString();
+                   // UserData user = new UserData(first, surname, )
+                }
+
+
+
                 startActivity(new Intent(DatabaseInputPersonalInfoActivity.this, DatabaseInputInsuranceInfoActivity.class));
                 DatabaseReference myRef = mFirebaseDatabase.getReference("message");
                 myRef.setValue("Hello, World!");
+                
+
             }
         });
     }
 
+
+
+    /*
+    Empty checking for fields
+     */
+
+    private boolean inputHandle(EditText editText) {
+
+        boolean empty = false;
+
+        if(editText.getText().toString().equals("")){
+            empty = true;
+        }
+        else {
+            empty = false;
+        }
+
+        return empty;
+
+    }
 
 
 
