a458cb4d25f58bf0b453a528a08fc54989ce6340
Valeria <vcasadoh@gmail.com>
2019-05-12 23:55:29 +0100

    View the cars inside my insurance information.

--- app/src/main/java/com/group2/carinsuranceapp/Fragment_AddCar.java
+++ app/src/main/java/com/group2/carinsuranceapp/Fragment_AddCar.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -77,7 +78,7 @@ public class Fragment_AddCar extends Fragment {
                         //Path is Car -> Current user ID -> Car ID
                         myRef.child(userAuth.getUid()).child(reg).setValue(car);
 
-                        startActivity(new Intent(getContext(), Fragment_MyInsuranceInfo.class));
+                        startActivity(new Intent(getContext(), LoggedInMainActivity.class));
 
                     } catch (Exception e) {
                         e.printStackTrace();
--- app/src/main/java/com/group2/carinsuranceapp/Fragment_MyInsuranceInfo.java
+++ app/src/main/java/com/group2/carinsuranceapp/Fragment_MyInsuranceInfo.java
@@ -6,12 +6,27 @@ import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.group2.databaseclasses.UserCar;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Fragment_MyInsuranceInfo extends Fragment {
 
@@ -21,29 +36,43 @@ public class Fragment_MyInsuranceInfo extends Fragment {
     private Button addCar;
     private ListView carList;
 
+    DatabaseReference databaseCars;
+    private FirebaseAuth mAuth;
+    private String userID;
+    FirebaseUser firebaseUser;
+    private static final String TAG = "DEBUGGING - ";
+
+
+
+
+    List<UserCar> listofcars;
+
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_my_insurance_info,null);
+
+
+
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-
+        //initialise layout widgets and database stuff
         carMake = view.findViewById(R.id.text_car_make_insurance_info_changable);
         carModel= view.findViewById(R.id.text_car_model_insurance_info_changeable);
         carRegNum= view.findViewById(R.id.text_car_reg_num_insurance_info_changeable);
         carList = view.findViewById(R.id.list_of_cars);
+        firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+        userID = firebaseUser.getUid();
+        databaseCars = FirebaseDatabase.getInstance().getReference("Car/" + userID);
 
 
 
-
-
-
-
-
+        listofcars = new ArrayList<>();
 
 
 
@@ -57,6 +86,29 @@ public class Fragment_MyInsuranceInfo extends Fragment {
 
                 fragmentTransaction.replace(R.id.screen,fragment);
                 fragmentTransaction.commit();
+            }
+        });
+
+        databaseCars.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                Log.e(TAG, "This exists  " + dataSnapshot.getChildrenCount());
+                for (DataSnapshot sn: dataSnapshot.getChildren()) {
+                    UserCar car = sn.getValue(UserCar.class);
+                    Log.e(TAG, "whatever getchildren() does: " + sn.toString());
+                    listofcars.add(car);
+                    Log.e(TAG, "What it's adding to the list" + car.getRegistration());
+
+                }
+
+                CarsList adapter = new CarsList(getActivity(), listofcars);
+                carList.setAdapter(adapter);
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
             }
         });
     }
